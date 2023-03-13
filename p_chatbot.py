e81309c18aa6fd9ffd70b4160a4f1377a59b9317
XHI-NM <chichini86@gmail.com>
2023-03-13 16:56:58 +0900

    Update p_chatbot.py

--- p_chatbot.py
+++ p_chatbot.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
+import os
+import psutil
 
 @st.cache_data
 def cached_model():
@@ -15,6 +17,19 @@ def get_dataset():
     df=pd.read_csv('wellness_dataset.csv')
     df['embedding']=df['embedding'].apply(json.loads)
     return df
+
+def check_usage_of_cpu_and_memory():
+    
+    pid = os.getpid()
+    py  = psutil.Process(pid)
+    
+    cpu_usage   = os.popen("ps aux | grep " + str(pid) + " | grep -v grep | awk '{print $3}'").read()
+    cpu_usage   = cpu_usage.replace("\n","")
+    
+    memory_usage  = round(py.memory_info()[0] /2.**30, 2)
+    
+    print("cpu usage\t:", cpu_usage, "%")
+    print("memory usage\t:", memory_usage, "%")
 
 model=cached_model()
 df=get_dataset()
@@ -38,6 +53,7 @@ if submitted and user_input:
     answer=df.loc[df['similarity'].idxmax()]
     st.session_state.past.append(user_input)
     st.session_state.generated.append(answer['챗봇'])
+    check_usage_of_cpu_and_memory()
 
 for i in range(len(st.session_state['past'])):
     message(st.session_state['past'][i], is_user=True, key=str(i)+'_user')
